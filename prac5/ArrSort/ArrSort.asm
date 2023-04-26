dae7e421dcb3182e40be9ac4c7f71592e70f8e66
GeraldFreis <GeraldFreis@github.com>
2023-04-26 16:34:27 +0930

    safe

--- prac5/ArrSort/ArrSort.asm
+++ prac5/ArrSort/ArrSort.asm
@@ -12,71 +12,122 @@ M=0
 @jval
 M=0
 
+
+@R1
+D=M
+@200
+M=D
+@201
+M=D
+
 (FIRSTLOOP) // this is a loop that iterates over all array items
 	// checking that we are in range
 	@firstIndex
 	D=M
 	@R2
-	D=D-M
+	D=M-D
 	@OUTOFRANGE // if (the firstIndex is > the length (R2): D will be < 0 and thus we will jump to OUTOFRANGE
-	D;JGT
-
-	// going to the current element
+	D;JLE
+	
+	
+	// setting the current addresses
 	@firstIndex
 	D=M
 	@R1
-	A=M+D // going to the address at R1 + index
-	D=A
-	@ival // setting the i val to the current element
+	A=M
+	D=A+D
+	@200
 	M=D
-	(SECONDlOOP) // this is the second loop makes my life a lot easie koopa
-		@secondIndex // going to the second index and checking range
-		D=M
-		@R2
-		D=D-M
-		@OUTOFRANGESECONDLOOP
-		D;JGT
+	@201
+	M=D+1
+	//@R1
+	//M=M+1
+	
+	// setting the current element
+	@200
+	D=M
+	A=D
+	D=M
+	@ival
+	M=D
+	
+	//@SECONDLOOP
+	//0;JMP
+	
+(SECONDlOOP) // this is the second loop makes my life a lot easie koopa
+	@secondIndex // going to the second index and checking range
+	D=M
+	@R2
+	D=M-D
+	@OUTOFRANGESECONDLOOP
+	D;JLE
 
-		// if we are fine
+	// if we are fine
 
-		// setting the second element
-		@secondIndex
-		D=M
-		@R1
-		A=M+D
-		D=A
-		@jval
-		M=D
+	// setting the second element
+	@201
+	D=M
+	A=D
+	D=M
+	@jval
+	M=D
+	
+	// setting the second address
+	//@secondIndex
+	@201
+	M=M+1
+	@secondIndex
+	M=M+1
+	// comparing the i and j vals
+	@jval
+	D=M
+	@ival
+	D=D-M // if the ival is > jval then D register will be negative 
+	@ISGREATER
+	D;JLE
+	
+	@SECONDLOOP
+	0;JMP
+		
 
-		// comparing the i and j vals
-		@jval
-		D=M
-		@ival
-		D=D-M // if the ival is > jval then D register will be negative 
-		@ISGREATER
-
-
-
+(ISGREATER)
+	// going to the firstIndex 
+	@jval
+	D=M
+	@200
+	A=M
+	M=D
+	
+	@ival
+	D=M
+	@201
+	A=M-1
+	M=D
+	
+	// changing the addresse
+	 
+	@SECONDLOOP
+	0;JMP
 
 
 (OUTOFRANGE) // this is basically an end
 	@R0 // setting R0 to true
-	D=M
-	D=D-1
-	M=D
+	M=M-1
+	@END
+	0;JMP
+(END)
+	@END
 	0;JMP
 
 (OUTOFRANGESECONDLOOP) // this is for when we have iterated across the array
+	@R1
+	M=M+1
 	@firstIndex // incrementing the first index
 	M=M+1
 	D=M
 	@secondIndex // resetting the second index to the first index's value
 	M=D+1
-
+	
 	@FIRSTLOOP // jumping back to the first 
 	0;JMP
-
-
-
-
 
