c9e38ed7a1ef4c018122912f31bd3a6f0923f6c1
Gerald Freislich <freislichg@gmail.com>
2023-05-07 09:44:47 +0930

    safe

--- prac5/ArrSort/ArrSort.asm
+++ prac5/ArrSort/ArrSort.asm
@@ -1,163 +1,144 @@
-@firstIndex
+@i // index of first loop
 M=0
-@ival
+@j // index of second loop
 M=0
-@secondIndex
-M=0
-@jval
-M=0
-
-
+// setting the first address
 @R1
 D=M
-@200
+@201 // random point in memory holding the first address
 M=D
-@201
+@202 // random point in memory holding the second address
 M=D
+@temp // temporary value to store
+M=0
 
-(FIRSTLOOP) // this is a loop that iterates over all array items
-	// checking that we are in range
-	@firstIndex
+(FIRSTLOOP)
+	// checking that we are still in bounds
+	@i
 	D=M
 	@R2
-	D=M-D
-	@OUTOFRANGE // if (the firstIndex is > the length (R2): D will be < 0 and thus we will jump to OUTOFRANGE)
-	D;JLE
-	
-	
-	// setting the current addresses
-	@firstIndex
-	D=M
-	@R1
-	A=M
-	D=A+D
-	@200
-	M=D
-	@201
-	M=D+1
-	//@R1
-	//M=M+1
-	
-	// setting the current element
-	@200
-	D=M
-	A=D
-	D=M
-	@ival
-	M=D
-	
-	//@SECONDLOOP
-	//0;JMP
-	
-(SECONDlOOP) // this is the second loop makes my life a lot easie koopa
-	@secondIndex // going to the second index and checking range
-	D=M
-	@R2
-	D=M-D
-	@OUTOFRANGESECONDLOOP
-	D;JLE
-
-	// if we are fine
-
-	// setting the second element
-	@201
-	D=M
-	A=D
-	D=M
-	@jval
-	M=D
-	
-	// setting the second address
-	//@secondIndex
-	@201
-	M=M+1
-	@secondIndex
-	M=M+1
-
-	// comparing the i and j vals
-	// checking if either one is negative
-	@jval
-	D=M
-	@JNEG // if jval is negative
-	D;JLT
-
-	// checking if I is negative
-	@ival
-	D=M
-	@SECONDLOOP
-	D;JLT
-
-
-	@jval
-	D=M
-	@ival
-	D=D-M // if the ival is > jval then D register will be negative 
-	@ISGREATER
-	D;JLE
-	
-	@SECONDLOOP
-	0;JMP
-
-(JNEG) 
-	// checking if ival is positive
-	@ival
-	D=M
-	@INEG // jumps if i is negative
-	D;JLT
-
-	// if only Jval is negative we know that ival is greater 
-	@ISGREATER
-	0;JMP
-
-	
-
-(INEG) // we are here if both are neg
-	// we can check if ival is greater by taking them away from each other
-	@ival
-	D=M
-	@jval
-	D=M-D // this will be positive if I is greater
-	@ISGREATER
-	D;JLE
-
-	@SECONDLOOP
-	0;JMP
-
-(ISGREATER)
-	// going to the firstIndex 
-	@jval
-	D=M
-	@200
-	A=M
-	M=D
-	
-	@ival
-	D=M
-	@201
-	A=M-1
-	M=D
-		 
-	@SECONDLOOP
-	0;JMP
-
-
-(OUTOFRANGE) // this is basically an end
-	@R0 // setting R0 to true
-	M=M-1
+	D=M-D // if this is <= 0 we go to end
 	@END
+	D;JLE
+
+	// setting up the address values
+	@201
+	D=M
+	@202 // next value
+	M=D+1
+
+	(SECONDLOOP)
+		// checking that we are in bounds
+		@j
+		D=M
+		@R2
+		D=M-D
+		@OUTOFBOUNDSINNER
+		D;JLE
+
+		@202
+		A=M
+		D=M
+		@COMPARENEGATIVES // if the value at the second address is < 0
+		D;JLT
+		// if we are still herer the second address is positive
+		@COMPAREPOSITIVES // we jump unconditionally to the negatives
+		0;JMP
+
+(GENERICSWAP)
+	@202 // storing second value in a temp spot
+	A=M
+	D=M
+
+	@temp
+	M=D
+	@201 // putting the first value into the second value spot
+	A=M
+	D=M
+	@202
+	A=M
+	M=D
+	
+	@temp
+	D=M
+	@201
+	A=M
+	M=D
+
+
+(INCREMENT)
+	@202 // incrementing the second address
+	M=M+1
+	@j
+	M=M+1
+	@SECONDLOOP
 	0;JMP
+
+
+
+(COMPARENEGATIVES)
+	// checking if the first value is negative too
+	@201
+	A=M
+	D=M
+	// if it is we can jump to the swap negatives
+	@SWAPNEGATIVES
+	D;JLT
+	// otherwise we know that second value is negative and first is positive so we just swap them
+	@GENERICSWAP
+	0;JMP
+
+(SWAPNEGATIVES)
+	// checking if the first value is > the second value
+	@201
+	A=M
+	D=M
+	@202
+	A=M
+	D=D-M // this will be positive if the first value is > than second
+	// if it is < 0 we can just skip
+	@INCREMENT
+	D;JLE
+	// otherwise if the val is positive first value is > than second
+	@GENERICSWAP
+	0;JMP
+
+(COMPAREPOSITIVES)
+	//checking if the first value is < 0 then we can just skip
+	@201
+	A=M
+	D=M
+	@INCREMENT
+	D;JLE
+	// we are here if first value is > 0
+	// now we check which one is greater than the other
+	@201
+	A=M
+	D=M
+	@202
+	A=M
+	D=M-D // this will be negative if the first value is > than the second
+	@GENERICSWAP
+	D;JLT
+	// otherwise we just increment
+	@INCREMENT
+	0;JMP
+
+(OUTOFBOUNDSINNER)
+	@201
+	M=M+1
+	@i
+	M=M+1
+	D=M
+	@j
+	M=D+1
+	@FIRSTLOOP
+	0;JMP
+
 (END)
+	@0
+	D=A
+	@R0
+	M=D-1
 	@END
-	0;JMP
-
-(OUTOFRANGESECONDLOOP) // this is for when we have iterated across the array
-	@R1
-	M=M+1
-	@firstIndex // incrementing the first index
-	M=M+1
-	D=M
-	@secondIndex // resetting the second index to the first index's value
-	M=D+1
-	
-	@FIRSTLOOP // jumping back to the first 
 	0;JMP
