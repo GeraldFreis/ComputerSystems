96839f8c84c36c2106d7ae763111ec4ce0af79e1
Gerald Freislich <freislichg@gmail.com>
2023-05-13 12:21:48 +0930

    safe commits

---
+++ prac6/VMTranslator/VMTranslator.py
@@ -0,0 +1,73 @@
+class VMTranslator:
+
+    def vm_push(segment, offset):
+        '''Generate Hack Assembly code for a VM push operation'''
+        return "" 
+
+    def vm_pop(segment, offset):
+        '''Generate Hack Assembly code for a VM pop operation'''
+        return ""
+
+    def vm_add():
+        '''Generate Hack Assembly code for a VM add operation'''
+        return ""
+
+    def vm_sub():
+        '''Generate Hack Assembly code for a VM sub operation'''
+        return ""
+
+    def vm_neg():
+        '''Generate Hack Assembly code for a VM neg operation'''
+        return ""
+
+    def vm_eq():
+        '''Generate Hack Assembly code for a VM eq operation'''
+        return ""
+
+    def vm_gt():
+        '''Generate Hack Assembly code for a VM gt operation'''
+        return ""
+
+    def vm_lt():
+        '''Generate Hack Assembly code for a VM lt operation'''
+        return ""
+
+    def vm_and():
+        '''Generate Hack Assembly code for a VM and operation'''
+        return ""
+
+    def vm_or():
+        '''Generate Hack Assembly code for a VM or operation'''
+        return ""
+
+    def vm_not():
+        '''Generate Hack Assembly code for a VM not operation'''
+        return ""
+
+    def vm_label(label):
+        '''Generate Hack Assembly code for a VM label operation'''
+        return ""
+
+    def vm_goto(label):
+        '''Generate Hack Assembly code for a VM goto operation'''
+        return ""
+
+    def vm_if(label):
+        '''Generate Hack Assembly code for a VM if-goto operation'''
+        return ""
+
+    def vm_function(function_name, n_vars):
+        '''Generate Hack Assembly code for a VM function operation'''
+        return ""
+
+    def vm_call(function_name, n_args):
+        '''Generate Hack Assembly code for a VM call operation'''
+        return ""
+
+    def vm_return():
+        '''Generate Hack Assembly code for a VM return operation'''
+        return ""
+
+
+
+        
