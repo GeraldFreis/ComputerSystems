a42f16fe21ba2790efddad1621b413c2bc7338ae
Gerald Freislich <freislichg@gmail.com>
2023-05-18 15:40:13 +0930

    safe

--- prac6/VMTranslator/VMTranslator.py
+++ prac6/VMTranslator/VMTranslator.py
@@ -40,8 +40,8 @@ class VMTranslator:
         return ""
 
     def vm_eq():
-
-        return ["@SP", "AM=M-1", "D=M", "A=A-1", "D=M-D", "@ISEQ"+str(self.counter), "D;JEQ", "@SP", "A=M-1", "M=0", "@END" + str(self.counter), "0;JMP", "(ISEQ"+str(self.counter)")", "@SP", "A=M-1", "M=-1", "(END"+str(self.counter)")"]    
+        return ""
+        # return ["@SP", "AM=M-1", "D=M", "A=A-1", "D=M-D", "@ISEQ"+str(self.counter), "D;JEQ", "@SP", "A=M-1", "M=0", "@END" + str(self.counter), "0;JMP", "(ISEQ"+str(self.counter)")", "@SP", "A=M-1", "M=-1", "(END"+str(self.counter)")"]    
 
     def vm_gt():
         print("@SP\nD=A\nD=M\nA=A-1")
@@ -88,49 +88,49 @@ class VMTranslator:
         '''Generate Hack Assembly code for a VM return operation'''
         return ""
 
-# # A quick-and-dirty parser when run as a standalone script.
-# if __name__ == "__main__":
-#     import sys
-#     if(len(sys.argv) > 1):
-#         with open(sys.argv[1], "r") as a_file:
-#             for line in a_file:
-#                 tokens = line.strip().lower().split()
-#                 if(len(tokens)==1):
-#                     if(tokens[0]=='add'):
-#                         print(VMTranslator.vm_add())
-#                     elif(tokens[0]=='sub'):
-#                         print(VMTranslator.vm_sub())
-#                     elif(tokens[0]=='neg'):
-#                         print(VMTranslator.vm_neg())
-#                     elif(tokens[0]=='eq'):
-#                         print(VMTranslator.vm_eq())
-#                     elif(tokens[0]=='gt'):
-#                         print(VMTranslator.vm_gt())
-#                     elif(tokens[0]=='lt'):
-#                         print(VMTranslator.vm_lt())
-#                     elif(tokens[0]=='and'):
-#                         print(VMTranslator.vm_and())
-#                     elif(tokens[0]=='or'):
-#                         print(VMTranslator.vm_or())
-#                     elif(tokens[0]=='not'):
-#                         print(VMTranslator.vm_not())
-#                     elif(tokens[0]=='return'):
-#                         print(VMTranslator.vm_return())
-#                 elif(len(tokens)==2):
-#                     if(tokens[0]=='label'):
-#                         print(VMTranslator.vm_label(tokens[1]))
-#                     elif(tokens[0]=='goto'):
-#                         print(VMTranslator.vm_goto(tokens[1]))
-#                     elif(tokens[0]=='if-goto'):
-#                         print(VMTranslator.vm_if(tokens[1]))
-#                 elif(len(tokens)==3):
-#                     if(tokens[0]=='push'): # pushing segments and then  which one to push (inbound checking?)
-#                         print(VMTranslator.vm_push(tokens[1],int(tokens[2])))
-#                     elif(tokens[0]=='pop'):
-#                         print(VMTranslator.vm_pop(tokens[1],int(tokens[2])))
-#                     elif(tokens[0]=='function'):
-#                         print(VMTranslator.vm_function(tokens[1],int(tokens[2])))
-#                     elif(tokens[0]=='call'):
-#                         print(VMTranslator.vm_call(tokens[1],int(tokens[2])))
+# A quick-and-dirty parser when run as a standalone script.
+if __name__ == "__main__":
+    import sys
+    if(len(sys.argv) > 1):
+        with open(sys.argv[1], "r") as a_file:
+            for line in a_file:
+                tokens = line.strip().lower().split()
+                if(len(tokens)==1):
+                    if(tokens[0]=='add'):
+                        print(VMTranslator.vm_add())
+                    elif(tokens[0]=='sub'):
+                        print(VMTranslator.vm_sub())
+                    elif(tokens[0]=='neg'):
+                        print(VMTranslator.vm_neg())
+                    elif(tokens[0]=='eq'):
+                        print(VMTranslator.vm_eq())
+                    elif(tokens[0]=='gt'):
+                        print(VMTranslator.vm_gt())
+                    elif(tokens[0]=='lt'):
+                        print(VMTranslator.vm_lt())
+                    elif(tokens[0]=='and'):
+                        print(VMTranslator.vm_and())
+                    elif(tokens[0]=='or'):
+                        print(VMTranslator.vm_or())
+                    elif(tokens[0]=='not'):
+                        print(VMTranslator.vm_not())
+                    elif(tokens[0]=='return'):
+                        print(VMTranslator.vm_return())
+                elif(len(tokens)==2):
+                    if(tokens[0]=='label'):
+                        print(VMTranslator.vm_label(tokens[1]))
+                    elif(tokens[0]=='goto'):
+                        print(VMTranslator.vm_goto(tokens[1]))
+                    elif(tokens[0]=='if-goto'):
+                        print(VMTranslator.vm_if(tokens[1]))
+                elif(len(tokens)==3):
+                    if(tokens[0]=='push'): # pushing segments and then  which one to push (inbound checking?)
+                        print(VMTranslator.vm_push(tokens[1],int(tokens[2])))
+                    elif(tokens[0]=='pop'):
+                        print(VMTranslator.vm_pop(tokens[1],int(tokens[2])))
+                    elif(tokens[0]=='function'):
+                        print(VMTranslator.vm_function(tokens[1],int(tokens[2])))
+                    elif(tokens[0]=='call'):
+                        print(VMTranslator.vm_call(tokens[1],int(tokens[2])))
 
-#         
+        
