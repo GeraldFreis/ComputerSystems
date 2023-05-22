c233a6a47f11b7f16eb4e314db40ea198da1501a
Gerald Freislich <freislichg@gmail.com>
2023-05-22 13:44:18 +0930

    safe

--- prac6/VMTranslator/VMTranslator.py
+++ prac6/VMTranslator/VMTranslator.py
@@ -100,11 +100,11 @@ class VMTranslator:
     def vm_function(function_name, n_vars):
         '''Generate Hack Assembly code for a VM function operation'''
         returnstring = str()
-        returnstring += f"({function_name})\n@LCL\nA=M\n"
+        returnstring += f"({function_name})\n"
         # setting all the locals to zero n shiz
         for i in range(0, n_vars):
-            returnstring += "M=0\nA=A+1\n"
-        returnstring += "D=A\n@LCL\nM=D\n"
+            returnstring += "@SP\nM=M+1\nA=M-1\nM=0\n"
+        # returnstring += "D=A\n@LCL\nM=D\n"
 
         return returnstring
 
